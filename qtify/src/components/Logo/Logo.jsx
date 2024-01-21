f93429690213dfc050f335cf16556ed735cc863b
Suhas  Lokesha <suhas@asicdesigners.com>
2024-01-21 18:40:25 +0530

    minor fix

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import {LogoImage} from "../../assets/logo.png"
 export default function Logo(){
 
   return(
-    <img src="../../assets/logo.png" alt="logo" width={67}/>
+    <img src={LogoImage} alt="logo" width={67}/>
   )
 }
