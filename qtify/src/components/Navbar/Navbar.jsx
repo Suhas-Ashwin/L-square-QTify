d8599671d98efad51f030d61d1c97c9dc154352f
Suhas  Lokesha <suhas@asicdesigners.com>
2024-01-21 17:41:01 +0530

    Fixing compilation error in Navbar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../Logo/Logo"
 import Search from "../Search/Search"
 import Button from "../Button/Button"
-import styles from "./Navbar/Navbar.module.css"
+import styles from "./Navbar.module.css"
 
 export default function Navbar(){
 
