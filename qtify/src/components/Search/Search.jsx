18442a2276ba0cdb8cdb13b3bbf1e3f20f3bd78c
Suhas  Lokesha <suhas@asicdesigners.com>
2024-01-21 17:44:53 +0530

    Fixing compilation error in Search.jsx

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Search.module.css";
-import {ReactComponent as SearchIcon} from "../../assets/searchicon.svg";
+import {ReactComponent as SearchIcon} from "../../assets/Search icon.svg";
 
 export default function Search(display){
 
