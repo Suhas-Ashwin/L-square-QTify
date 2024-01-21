0e50370a000eeaafd5124c53924f9d4da976729c
Suhas  Lokesha <suhas@asicdesigners.com>
2024-01-21 18:07:30 +0530

    Changing logo

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function Logo(){
 
   return(
-    <img src="../../assets/Group 7478.svg" alt="logo" width={67}/>
+    <img src="../../assets/Group 3748.png" alt="logo" width={67}/>
   )
 }
